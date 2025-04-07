f49f98d0df82582e2826a4bcd5fa5a13ed1cb079
Tawdi <ahmedtawdi00@gmail.com>
2025-04-07 15:05:49 +0100

    modifier model Newsletter

--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Newsletter extends Model
 {
-    //
+    protected $fillable = ['user_id', 'title', 'content', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function campaigns()
+    {
+        return $this->hasMany(Campaign::class);
+    }
 }

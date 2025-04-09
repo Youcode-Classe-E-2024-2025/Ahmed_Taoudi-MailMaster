5d7b45e77a83933858f0f9ff35b019ab40a8eb03
Tawdi <ahmedtawdi00@gmail.com>
2025-04-09 10:17:27 +0100

    ajoute service pour newsletter

--- app/Services/NewsletterService.php
+++ app/Services/NewsletterService.php
@@ -2,4 +2,63 @@
 
 namespace App\Services;
 
-class NewsletterService{}
+use App\Http\Requests\NewsletterRequest;
+use App\Models\Newsletter;
+use Illuminate\Support\Facades\Auth;
+
+class NewsletterService
+{
+    // Get all newsletters
+    public function getAll()
+    {
+        return Newsletter::all();
+    }
+
+    // Get a single newsletter by ID
+    public function getById($id)
+    {
+        return Newsletter::find($id);
+    }
+
+    // Create a new newsletter
+    public function create(NewsletterRequest $request)
+    {
+
+        $validatedData = $request->validated();
+
+        $validatedData['user_id'] = Auth::id();
+
+        $newsletter = Newsletter::create($validatedData);
+
+        return $newsletter;
+    }
+
+    // Update an existing newsletter
+    public function update($id, NewsletterRequest $request)
+    {
+        $newsletter = $this->getById($id);
+
+        if (!$newsletter) {
+            return null;
+        }
+
+        $validatedData = $request->validated(); 
+
+        $newsletter->update($validatedData);
+
+        return $newsletter;
+    }
+
+    // Delete a newsletter
+    public function delete($id)
+    {
+        $newsletter = $this->getById($id);
+
+        if (!$newsletter) {
+            return null;
+        }
+
+        $newsletter->delete();
+        return true;
+    }
+}

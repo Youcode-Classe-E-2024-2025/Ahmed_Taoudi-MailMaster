f1a80e7512cee34f82816768b183ab459e351056
Tawdi <ahmedtawdi00@gmail.com>
2025-04-09 12:15:01 +0100

    modifier model subscriber

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -6,10 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscriber extends Model
 {
-    protected $fillable = ['email', 'name', 'newsletter_id', 'status'];
+    protected $fillable = ['email','name','status'];
 
     public function campaigns()
     {
         return $this->belongsToMany(Campaign::class, 'campaign_subscriber');
     }
+
+    public function scopeActive($query)
+    {
+        return $query->where('status', true);
+    }
+
+
+    public function scopeInactive($query)
+    {
+        return $query->where('status', false);
+    }
+
 }

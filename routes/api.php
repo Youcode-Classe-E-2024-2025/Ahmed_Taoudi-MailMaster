76df1b3e881f90cc846f2ef896b412b8248a8721
Tawdi <ahmedtawdi00@gmail.com>
2025-04-09 10:48:52 +0100

    ajoute des routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NewsletterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//  Auth
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+
+Route::middleware('auth:sanctum')->group(function(){
+    
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::apiResource('newsletter',NewsletterController::class);
+});

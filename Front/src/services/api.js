e51fe5a73818cb6eb0b4fd1e10a7994e32d8ecdc
Tawdi <ahmedtawdi00@gmail.com>
2025-04-16 10:57:59 +0100

    ajoute de axios config

---
+++ Front/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const token = localStorage.getItem('token');
+if (token) {
+  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
+export default api;
